57de462ffb53e87836c389eadce25dbdf2d66720
Jacob Place <jake.place@gmail.com>
2023-07-13 10:23:15 -0400

    Dotfiles

---
+++ nvim/lua/core/keymaps.lua
@@ -0,0 +1,43 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+vim.opt.backspace = '2'
+vim.opt.showcmd = true
+vim.opt.laststatus = 2
+vim.opt.autowrite = true
+vim.opt.cursorline = true
+vim.opt.autoread = true
+
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.shiftround = true
+vim.opt.expandtab = true
+
+vim.wo.number = true
+vim.wo.relativenumber = true
+vim.opt.colorcolumn = '80'
+vim.opt.ignorecase = true
+vim.wo.cursorline = true
+vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+
+--[[
+set nocompatible            " disable compatibility to old-time vi
+set showmatch               " show matching 
+set mouse=v                 " middle-click paste with 
+set hlsearch                " highlight search 
+set incsearch               " incremental search
+set tabstop=4               " number of columns occupied by a tab 
+set softtabstop=4           " see multiple spaces as tabstops so <BS> does the right thing
+set expandtab               " converts tabs to white space
+set shiftwidth=4            " width for autoindents
+set autoindent              " indent a new line the same amount as the line just typed
+set number relativenumber   " add line numbers
+set wildmode=longest,list   " get bash-like tab completions
+set cc=80                   " set an 80 column border for good coding style
+filetype plugin indent on   "allow auto-indenting depending on file type
+syntax on                   " syntax highlighting
+set mouse=a                 " enable mouse click
+set clipboard=unnamedplus   " using system clipboard
+filetype plugin on
+set cursorline              " highlight current cursorline
+set ttyfast                 " Speed up scrolling in Vim]]--
